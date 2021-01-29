281dcde13a6fdfa7bff548f6410fd254fcde3e7b
Magnus siiftun1857 Frankline <39226927@qq.com>
2021-01-29 21:46:41 +0800

    把库和C#项目捞回来了

---
+++ Source/one/CompSelfHealOvertime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace Sayers1
+{
+    public class CompSelfHealOvertime : ThingComp
+    {
+
+    }
+    public class CompProperties_SelfHealOvertime : CompProperties
+    {
+        public double detlaHpPerSec = 0;
+        public int ticksBetweenHeal = -1;
+        public CompProperties_SelfHealOvertime() : base(typeof(CompSelfHealOvertime))
+        {
+        }
+        public CompProperties_SelfHealOvertime(Type cc) : base(cc == typeof(ThingComp) ? typeof(CompSelfHealOvertime) : cc)
+        {
+        }
+    }
+}
